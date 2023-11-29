355ec4f3dd345570b504d9ed38bd0819b3997172
Bigizic <olalekanisaac75@gmail.com>
2023-11-29 00:00:41 +0000

    add last mandatory file

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,14 @@
+export default function createReportObject(employeesList) {
+  const getNumberOfDepartments = (employeesList) => {
+    return Object.keys(employeesList).length;
+  };
+
+  const reportObject = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments,
+  };
+
+  return reportObject;
+}
