bc9f4eb3b8d7ce86c916ac9576405a39be8cffd9
Bigizic <olalekanisaac75@gmail.com>
2023-11-29 00:42:14 +0000

    updates

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,8 +1,8 @@
-export default function createIteratorObject (report) {
-  return (function * _ () {
-    for (const departMents of Object.values(report.allEmployees)) {
-      for (const emploYEE of departMents) {
-        yield emploYEE;
+export default function createIteratorObject(report) {
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
       }
     }
   }());
