cbdc46f54f2dd3163094c44ae05896be2b52ddd3
Bigizic <olalekanisaac75@gmail.com>
2023-11-30 22:19:44 +0000

    update task 3

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    promise
-      .then((response) => {
-        console.log('Got a response from the API');
-        resolve({ status: 200, body: 'success' });
-      })
-      .catch((error) => {
-        console.log('Got a response from the API');
-        reject(new Error());
-      });
+  return promise.then((response) => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  }).catch((error) => {
+    console.log('Got a response from the API');
+    return new Error();
   });
 }
