85c5af30955f06dbcfbf10d257a69fdae939c36f
Bigizic <olalekanisaac75@gmail.com>
2023-12-01 03:36:14 +0000

    add all updates

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,22 @@
+import Car from './10-car.js';
+
+export default class EvCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    const myModel = super.constructor[Symbol.newModel];
+
+    return new myModel();
+  }
+}
