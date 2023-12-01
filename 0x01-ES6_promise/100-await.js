0aa77776bc599a2c946814436657cc4354a8b6bd
Bigizic <olalekanisaac75@gmail.com>
2023-12-01 02:11:52 +0000

    updates

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
@@ -9,4 +9,3 @@ export default async function asyncUploadUser() {
     return { photo: null, user: null };
   }
 }
-
