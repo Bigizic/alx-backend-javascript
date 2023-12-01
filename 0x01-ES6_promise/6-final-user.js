b7961a65b2fa8b533a70662f1b634fd0e90004ca
Bigizic <olalekanisaac75@gmail.com>
2023-12-01 00:46:30 +0000

    add files

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,17 @@
-import { signUpUser } from "./4-user-promise";
+import signUpUser from "./4-user-promise.js";
+
+import uploadPhoto from "./5-photo-reject.js";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const filePromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([userPromise, filePromise]).then((rest) => {
+    return rest.map((result) => {
+      return {
+        status: result.status,
+        value: result.status === "fulfilled" ? result.value : result.reason,
+      };
+    });
+  });
+}
