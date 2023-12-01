fb47a89147fefef1f8088e36cb8390bfe715d6fc
Bigizic <olalekanisaac75@gmail.com>
2023-12-01 02:16:31 +0000

    Update make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,5 +2,5 @@ import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const size = [19, 20, 34];
-  return size.map(siz => new ClassRoom(siz));
+  return size.map((siz) => new ClassRoom(siz));
 }
