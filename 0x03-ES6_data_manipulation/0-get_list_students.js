ac0d8737c133b26517dce1a5b452339794198677
Bigizic <olalekanisaac75@gmail.com>
2023-12-04 13:20:41 +0000

    new files

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,7 +4,7 @@ export default function getListStudents() {
   const items = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 3, firstName: 'Serena', location: 'San Francisco' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
   return items;
 }
--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,5 @@
-export default function getStudentsByLocation{
-  
+/* returns an array of objects who are located in a specific city */
+
+export default function getStudentsByLocation(students, locations){
+  return students.filter((x) => x.location === locations);
 }
