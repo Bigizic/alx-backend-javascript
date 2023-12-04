9043cd0b61056cd11532b8e13c0de779594c91f0
Bigizic <olalekanisaac75@gmail.com>
2023-12-04 11:45:24 +0000

    updaets

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,9 +2,9 @@
 
 export default function getListStudents() {
   const items = [
-    {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-    {id: 2, firstName: 'James', location: 'Columbia'},
-    {id: 3, firstName: 'Serena', location: 'San Francisco'},
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 3, firstName: 'Serena', location: 'San Francisco' },
   ];
   return items;
 }
