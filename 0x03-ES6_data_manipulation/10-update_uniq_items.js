df638857df04eace0889711d824ff16d5aa06e44
Bigizic <olalekanisaac75@gmail.com>
2023-12-05 23:54:59 +0000

    Update update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,19 @@
+/**
+ * updateUniqueItems: returns an updated map for all items with initial
+ * quantity at 1
+ *
+ * @param: {oldMap} - a map
+ *
+ * Return: a new map
+ *
+ * @author: Isaac <github.com/Bigizic>
+ */
+
 export default function updateUniqueItems(oldMap) {
   let updated = false;
+  if (!(oldMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
 
   oldMap.forEach((value, key) => {
     if (value === 1) {
