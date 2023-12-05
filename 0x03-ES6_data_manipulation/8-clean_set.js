042b5fbb56260cbd4bd693d76356a44ddcc98f0d
Bigizic <olalekanisaac75@gmail.com>
2023-12-05 16:33:35 +0000

    Update clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0) {
-    return '';
-  }
   const newSet = Array.from(set);
-  const res = newSet.filter((e) => e.startsWith(startString));
-
-  const result = res.map((v) => v.slice(startString.length)).join('-');
-
-  return result;
+  if (startString.length > 0) {
+    const res = newSet.filter((e) => e.startsWith(startString));
+    return res.map((v) => v.slice(startString.length)).join('-');
+  }
 }
