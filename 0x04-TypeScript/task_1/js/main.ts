7bd74d9232d67fe16d78d59a6ad024041a17ddf8
Bigizic <olalekanisaac75@gmail.com>
2023-12-07 00:27:06 +0000

    Update xTypeScript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,44 @@ interface Teacher {
   [key: string]: any;
 }
 
+interface Directors extends Teacher {
+  readonly numberOfReports: number;
+}
+
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassMethods;
+}
+
+
+interface StudentClassMethods {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+
+class StudentClass implements StudentClassMethods {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+
 const onlyTeacher: Teacher = {
   firstName: 'John',
   lastName: 'Wick',
@@ -16,5 +54,23 @@ const onlyTeacher: Teacher = {
   contract: true,
 };
 
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
 
-console.log(onlyTeacher);
+
+const student: StudentConstructor = StudentClass;
+const newStudent = new student("John", "Doe");
+
+console.log(newStudent.workOnHomework()); // Output: "Currently working"
+console.log(newStudent.displayName()); // Output: "John"
+
+//console.log(printTeacher("Ajohn", "Adam"))
+
+//console.log(director1);
+
+//console.log(onlyTeacher);
