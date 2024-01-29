34a7f8179f202376c4a1081eaf6c1339d139b5e0
Bigizic <olalekanisaac75@gmail.com>
2024-01-29 22:45:26 +0000

    add files

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,3 +1,7 @@
+/**
+ * stdinProcess - reads stdin and outputs what's entered
+ */
+
 function stdinProcess() {
   process.stdin.setEncoding('utf-8');
   process.stdout.write('Welcome to Holberton School, what is your name?\n');
