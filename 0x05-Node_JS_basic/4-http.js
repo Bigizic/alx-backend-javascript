2bd37f67e2b6d92d052378eb1c81369df5be540d
Bigizic <olalekanisaac75@gmail.com>
2024-01-30 02:28:10 +0000

    Update http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,18 @@
+/**
+ * A simple HTTP server using Node's HTTP module
+ *
+ * @author: {Isaac Ajibola  github.com/Bigizic}
+ */
+
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  if (req.url) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  }
+}).listen(1245, '0.0.0.0', () => {
+  console.log('Server is listening on port 1245');
+});
+
+module.exports = app;
