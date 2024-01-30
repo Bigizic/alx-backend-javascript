4fdee499f2f61205b15183f9ee48870b58586407
Bigizic <olalekanisaac75@gmail.com>
2024-01-30 22:40:23 +0000

    Update stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,18 @@
-const process = require('process');
+const pro = require('process');
 
 /* reads stdin and outputs what's entered
 */
 
-const app = ((process) => {
-  process.stdin.setEncoding('utf-8');
-  process.stdout.write('Welcome to Holberton School, what is your name?\n');
-  process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-    if (chunk) { process.stdout.write(`Your name is: ${chunk}`); }
+const app = ((pro) => {
+  pro.stdin.setEncoding('utf-8');
+  pro.stdout.write('Welcome to Holberton School, what is your name?\n');
+  pro.stdin.on('readable', () => {
+    const chunk = pro.stdin.read();
+    if (chunk) { pro.stdout.write(`Your name is: ${chunk}`); }
   });
-  process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  pro.stdin.on('end', () => {
+    pro.stdout.write('This important software is now closing\n');
   });
-})(process);
+})(pro);
 
 module.exports = app;
