1c56ca821b59a3f1a5d89df45072338c2920e9ae
Bigizic <olalekanisaac75@gmail.com>
2024-02-01 18:15:40 +0000

    add updates

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,5 +1,5 @@
 class Utils {
-    calculateNumber(type, a, b) {
+    static calculateNumber(type, a, b) {
         if (type === 'SUM') {
             return Math.round(a) + Math.round(b);
         } else if (type === 'DIVIDE') {
@@ -9,6 +9,7 @@ class Utils {
         } else if (type === 'SUBTRACT') {
             return Math.round(a) - Math.round(b);
         }
+        return 0;
     }
 }
 
