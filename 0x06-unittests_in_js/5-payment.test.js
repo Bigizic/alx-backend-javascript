bea400c334bbf9025579145c2edc1d822a9a43e2
Bigizic <olalekanisaac75@gmail.com>
2024-02-01 23:02:29 +0000

    add updates

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,32 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment'); // Assuming the correct file path
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberStub;
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+
+  it('should log the correct message with total 120 and called once', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+  });
+
+  it('should log the correct message with total 20 and called once', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+  });
+});
