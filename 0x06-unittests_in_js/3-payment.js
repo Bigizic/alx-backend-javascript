6281765167a29bce8e5cbd89ad2e262e393d6aa0
Bigizic <olalekanisaac75@gmail.com>
2024-02-01 18:11:55 +0000

    add updates

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -10,3 +10,4 @@ function sendPaymentRequestToApi(totalAmount, totalShipping) {
     const res = utils.calculateNumber('SUM', totalAmount, totalShipping);
     console.log(`The total is: ${res}`);
 }
+module.exports = sendPaymentRequestToApi;
