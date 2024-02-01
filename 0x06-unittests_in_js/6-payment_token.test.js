3dbc6ffeb0cadc12000c47d7e8f6f6404bc38a21
Bigizic <olalekanisaac75@gmail.com>
2024-02-01 23:51:22 +0000

    add up

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,25 +1,12 @@
 const { expect } = require('chai');
-const sinon = require('sinon');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
   it('should return a resolved promise with the correct object when success is true', (done) => {
     getPaymentTokenFromAPI(true).then((result) => {
-      expect(result).to.deep.equal({ data: 'Successful response from the API' });
+      expect(result).to.deep.equal({data: 'Successful response from the API'});
 
       done();
-    }).catch(done);
-  });
-
-  it('should return a promise that does nothing when success is false', (done) => {
-    const promiseConstructorStub = sinon.stub(global, 'Promise');
-
-    getPaymentTokenFromAPI(false).then(() => {
-      expect(promiseConstructorStub.notCalled).to.be.true;
-
-      done();
-    }).catch(done).finally(() => {
-      promiseConstructorStub.restore();
     });
   });
 });
