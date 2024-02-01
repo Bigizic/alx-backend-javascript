b50fbf307ef238c9310ba539a5051ebc557fcdac
Bigizic <olalekanisaac75@gmail.com>
2024-02-01 23:46:06 +0000

    app

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,7 +1,6 @@
 function getPaymentTokenFromAPI (success) {
   if (success) {
-    return Promise.resolve({ 'data': 'Successful response from the API' });
+    return Promise.resolve({ data: 'Successful response from the API' });
   }
-  return new Promise(() => {});
 }
 module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -5,7 +5,7 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 describe('getPaymentTokenFromAPI', () => {
   it('should return a resolved promise with the correct object when success is true', (done) => {
     getPaymentTokenFromAPI(true).then((result) => {
-      expect(result).to.deep.equal({ 'data': 'Successful response from the API' });
+      expect(result).to.deep.equal({ data: 'Successful response from the API' });
 
       done();
     }).catch(done);
