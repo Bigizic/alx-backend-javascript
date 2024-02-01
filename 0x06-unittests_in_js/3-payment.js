6c128144abcd8fbd666a19aebb6d1106d6818e27
Bigizic <olalekanisaac75@gmail.com>
2024-02-01 23:14:28 +0000

    add updates

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,12 +2,12 @@ const Utils = require('./utils');
 
 /**
  * sendPaymentRequestToApi - Sends payment to API
- * @param {int} totalAmount 
- * @param {str} totalShipping 
+ * @param {int} totalAmount
+ * @param {str} totalShipping
  */
 
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-    console.log(`The total is: ${res}`);
+function sendPaymentRequestToApi (totalAmount, totalShipping) {
+  const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${res}`);
 }
 module.exports = sendPaymentRequestToApi;
